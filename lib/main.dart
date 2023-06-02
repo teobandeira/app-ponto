eb5b00c84ca3cb0614b9eb5682cbef6d6b0d62a1
Teo Bandeira <timoteo.bandeira@gmail.com>
2023-06-02 09:36:05 -0300

    First Comiit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Registro de Ponto',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Registro de Ponto'),
+      ),
+      body: const Center(
+        child: Text('Aplicativo de Ponto de Entrada e Saída do Trabalho'),
+      ),
+    );
+  }
+}
